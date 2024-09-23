02353a32ff66deaa01a12ad0eb7d8a028d2fde85
Baldo788 <osvaldodelacruzportesgil@gmail.com>
2024-09-23 07:19:45 -0600

    Add files via upload

---
+++ Boceto/Menu.js
@@ -0,0 +1,11 @@
+function comprar() {
+    alert("Producto comprado");
+}
+
+function agregarCarrito() {
+    alert("Producto agregado al carrito");
+}
+
+function agregarFavoritos() {
+    alert("Producto agregado a favoritos");
+}
